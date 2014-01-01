e4c0e701e47b76f47bd17321270675fdfaccfbae
y_uuki <yuki.tsubo@gmail.com>
2014-01-01 15:14:43 +0900

    Fix

--- osx-homebrew.bash
+++ osx-homebrew.bash
@@ -2,21 +2,18 @@
 
 echo "Homebrew setting"
 
-gem install brewdler
-
-if [[ -x $(/usr/bin/which -s gcc) ]]; then
+if [[ ! "$(type gcc > /dev/null 2>&1)" ]]; then
     echo "Not Found MacOS X Command Line Tools"
     exit 1
 fi
-
-if [[ -x $(/usr/bin/which -s brew) ]]; then
+if [[ ! "$(type brew > /dev/null 2>&1)" ]]; then
     echo "Install Homebrew ..."
     sudo mkdir /usr/local
     sudo chmod 755 /usr/local
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
-gem install brewdler
+sudo gem install brewdler
 
 BREW="/usr/local/bin/brew"
 
